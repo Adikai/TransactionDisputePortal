ee041b12c36f9ae8cb06c6252dacd07fac760a1c
Adikai <adhil.sewrathan@gmail.com>
2026-09-01 22:38:28 +0200

    Updated stored Procs

--- TransactionDisputePortalApp/SQLScripts/StoredProcs.sql
+++ TransactionDisputePortalApp/SQLScripts/StoredProcs.sql
@@ -4,7 +4,7 @@ SET ANSI_PADDING ON;
 SET CONCAT_NULL_YIELDS_NULL ON;
 SET ARITHABORT ON;
 GO
-USE [TransactionDispute];
+USE TransactionDispute;
 GO
 --==============================================================================
 --Author:  Adhil Sewrathan
@@ -12,7 +12,7 @@ GO
 --Description:  Gets All transactions for a given account number and date range
 --==============================================================================
 
-CREATE OR ALTER PROCEDURE [dbo].[GetTransactionsByAccountAndDateRange]
+CREATE OR ALTER PROCEDURE dbo.GetTransactionsByAccountAndDateRange
     @AccountNumber NVARCHAR(20),
     @StartDate DATETIMEOFFSET,
     @EndDate DATETIMEOFFSET
@@ -36,4 +36,120 @@ BEGIN
       AND T.TransactionDate >= @StartDate 
       AND T.TransactionDate < @EndDate;
 END;
+GO
+--==============================================================================
+--Author:  Adhil Sewrathan
+--DateCreated: 2026-08-31
+--Description:  Creates a new dispute for a given transaction
+--==============================================================================
+CREATE OR ALTER PROCEDURE dbo.CreateDispute 
+@TransactionID BIGINT,
+@customerID INT,
+@DisputeStatusID INT,
+@ReasonCategory NVARCHAR(100),
+@CustomerNotes NVARCHAR(1000),
+@DisputedAmount DECIMAL(18, 2) 
+AS 
+BEGIN
+SET
+    NOCOUNT ON;
+
+INSERT INTO
+        dbo.Disputes (
+        TransactionID,
+        CustomerID,
+        DisputeStatusID,
+        ReasonCategory,
+        CustomerNotes,
+        DisputedAmount,
+        CreatedAt,
+        UpdatedAt
+    )
+VALUES
+    (
+        @TransactionID,
+        @customerID,
+        @DisputeStatusID,
+        @ReasonCategory,
+        @CustomerNotes,
+        @DisputedAmount,
+        SYSDATETIMEOFFSET(),
+        NULL
+    );
+
+END;
+
+GO
+--==============================================================================
+--Author:  Adhil Sewrathan
+--DateCreated: 2026-09-01
+--Description:  Creates a new dispute for a given transaction
+--==============================================================================
+CREATE OR ALTER PROCEDURE dbo.InsertAuditLog
+@DisputeID INT,
+@PreviousStatusID INT,
+@NewStatusID INT,
+@ChangedByStaffID INT,
+@ChangedByCustomerID INT,
+@Notes NVARCHAR(1000)
+
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO dbo.DisputeAuditLogs
+           (DisputeID
+           ,PreviousStatusID
+           ,NewStatusID
+           ,ChangedByStaffID
+           ,ChangedByCustomerID
+           ,Notes
+           ,[Timestamp])
+     VALUES(
+           @DisputeID,
+           @PreviousStatusID,
+           @NewStatusID,
+           @ChangedByStaffID,
+           @ChangedByCustomerID,
+           @Notes,
+           SYSDATETIMEOFFSET())
+
+END
+GO
+--==============================================================================
+--Author:  Adhil Sewrathan
+--DateCreated: 2026-09-01
+--Description:  Updates the status of a given dispute
+--==============================================================================
+CREATE OR ALTER PROCEDURE dbo.UpdateDisputeStatus
+@DisputeID INT,
+@NewStatusID INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE dbo.Disputes
+    SET DisputeStatusID = @NewStatusID,
+    UpdatedAt = SYSDATETIMEOFFSET()
+    WHERE DisputeID = @DisputeID
+    AND DisputeStatusID <> @NewStatusID;
+
+END
+GO
+--==============================================================================
+--Author:  Adhil Sewrathan
+--DateCreated: 2026-09-01
+--Description:  Updates the Balance of an account 
+--==============================================================================
+CREATE OR ALTER PROCEDURE dbo.UpdateAccountBalance
+@AccountID INT,
+@NewBalance DECIMAL(18, 2)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE dbo.Accounts
+    SET Balance = @NewBalance
+    WHERE AccountID = @AccountID;
+
+END
 GO
